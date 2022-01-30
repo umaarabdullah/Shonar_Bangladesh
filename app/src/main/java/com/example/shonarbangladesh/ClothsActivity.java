de286ed4eeea28ae4374dfdc25c7b75bb947e2e8
Umaar <umaar71@gmail.com>
2022-01-30 13:45:45 +0600

    linking views

--- app/src/main/java/com/example/shonarbangladesh/ClothsActivity.java
+++ app/src/main/java/com/example/shonarbangladesh/ClothsActivity.java
@@ -12,15 +12,21 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ClothsActivity extends AppCompatActivity {
 
     private String TAG = "ClothsActivity";
+    private int inputPrice = 0;
+    private double outputPrice = 0.00;
+    private TextView outputPriceTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cloths);
+
 
         // set listener to the arrow buttons to navigate between screens
         Button rightArrowButton = findViewById(R.id.clothsActivityRightArrow);
@@ -41,6 +47,47 @@ public class ClothsActivity extends AppCompatActivity {
             }
         });
 
+
+        // linking the input components and calculating vat and observing/listening user clicks
+        EditText inputBigShowPrice = findViewById(R.id.bigShowInputPrice);
+
+        inputBigShowPrice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setInputPrice(Integer.parseInt(inputBigShowPrice.getText().toString()));
+                calulateVatIncludedPriceForBigShowroom(inputPrice);
+                Log.d(TAG, "onClick: output price " + Double.toString(getOutputPrice()));
+                outputPriceTextView = findViewById(R.id.outputClothPriceTK);
+                outputPriceTextView.setText( (String) Double.toString(getOutputPrice()));
+            }
+        });
+
+        EditText inputOnlineShopPrice = findViewById(R.id.onlineShopInputPrice);
+
+        inputOnlineShopPrice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setInputPrice(Integer.parseInt(inputOnlineShopPrice.getText().toString()));
+                calulateVatIncludedPriceForOnlineShop(inputPrice);
+                outputPriceTextView = findViewById(R.id.outputClothPriceTK);
+                outputPriceTextView.setText( (String) Double.toString(getOutputPrice()));
+            }
+        });
+
+    }
+
+    private void calulateVatIncludedPriceForBigShowroom(int inputPrice) {
+
+        double vatInclPrice = inputPrice;
+        vatInclPrice = vatInclPrice + vatInclPrice*0.075;
+        setOutputPrice(vatInclPrice);
+    }
+
+    private void calulateVatIncludedPriceForOnlineShop(int inputPrice) {
+
+        double vatInclPrice = inputPrice;
+        vatInclPrice = vatInclPrice + vatInclPrice*0.05;
+        setOutputPrice(vatInclPrice);
     }
 
     @Override
@@ -79,5 +126,21 @@ public class ClothsActivity extends AppCompatActivity {
                 // code block
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public int getInputPrice() {
+        return inputPrice;
+    }
+
+    public void setInputPrice(int inputPrice) {
+        this.inputPrice = inputPrice;
+    }
+
+    public double getOutputPrice() {
+        return outputPrice;
+    }
+
+    public void setOutputPrice(double outputPrice) {
+        this.outputPrice = outputPrice;
     }
 }
